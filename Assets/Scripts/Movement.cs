bba62cc63ace351a63168a26bf1309a4882983d1
Elton Silva <eltoncarsilva73@gmail.com>
2021-09-17 11:48:43 -0300

    player basic movements

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [NonSerializedAttribute] public Vector3 direction;
+    [SerializeField] float speed = 1.0f;
+    void Start()
+    {
+        direction = Vector3.zero;
+    }
+
+    void Update()
+    {
+        transform.position += direction * Time.deltaTime * speed;
+    }
+}
---
+++ Assets/Scripts/PlayerInputs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputs : MonoBehaviour
+{
+    [SerializeField] Movement movement;
+    void Update()
+    {
+        input();
+    }
+
+    void input(){
+        movement.direction = Vector3.zero;
+        float horizontal = (float)Math.Round(Input.GetAxis("Horizontal"));
+        float vertical = (float)Math.Round(Input.GetAxis("Vertical"));
+
+        movement.direction = new Vector3(Math.Sign(horizontal), Math.Sign(vertical), 0);
+    }
+}
