36e2db80b73c9efa399548729f1ef41bbc899990
Elton Silva <eltoncarsilva73@gmail.com>
2021-09-19 00:59:58 -0300

    enemy run away from player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,11 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public bool hasFlag = false;
     [SerializeField] Movement movement;
     Rigidbody2D rigidBody;
+    public static Player Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
 
     void Start()
     {
